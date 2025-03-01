5de2dba1ac936b2c2980a7ec2a58874794b185eb
pivotiiii <17112987+pivotiiii@users.noreply.github.com>
2025-03-01 02:58:54 +0100

    add share image

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import {createFileRoute} from "@tanstack/react-router";
 import {SECCComponent} from "./-components/SECCComponent";
+import og_image from "./-assets/og_image_secc.png?format=webp&imagetools";
 
 const urlRoute = "/simultaneous_equation_cannon_calculator/";
 const title = "Simultaneous Equation Cannons - Calculator";
@@ -14,15 +15,15 @@ export const Route = createFileRoute(urlRoute)({
             {title: title},
             {name: "description", content: description},
             {property: "og:title", content: title},
-            // {property: "og:image", content: title},
+            {property: "og:image", content: og_image},
             {property: "og:type", content: "website"},
             {property: "og:url", content: __URL__ + urlRoute.slice(0, -1)},
             {property: "og:site_name", content: "pivotiiii"},
             {property: "og:description", content: description},
-            {name: "twitter:card", content: "summary"},
+            {name: "twitter:card", content: "summary_large_image"},
             {name: "twitter:title", content: title},
             {name: "twitter:description", content: description},
-            // {name: "twitter:image", content: ""},
+            {name: "twitter:image", content: og_image},
         ],
     }),
 });
